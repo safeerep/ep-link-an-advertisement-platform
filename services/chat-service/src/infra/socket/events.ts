30b84a17b3a16d2cc1067c52d834c57da8ea032f
safeerep <safeerep77@gmail.com>
2024-02-05 08:43:31 +0530

    call-events added to chat-service

--- services/chat-service/src/infra/socket/events.ts
+++ services/chat-service/src/infra/socket/events.ts
@@ -11,10 +11,11 @@ export const handleSocketEvents = ( socket: Socket) => {
 
     socket.on("join-room", ( roomId: string, userId: string) => {
         socket.join(roomId)
-        console.log(`user joined in a room`);
+        console.log('user joined in a room');
     })
+
     socket.on("send-message", async ( data: any) => {
-        console.log(`ok in backend send message event happened`);
+        console.log('ok in backend send message event happened');
         console.log(data);
         const isBlocked: boolean | any = await checkIsReceiverBlockedController(data);
         if (!isBlocked) {
@@ -24,10 +25,37 @@ export const handleSocketEvents = ( socket: Socket) => {
             socket.in(data?.chatRoomId).emit("receiver-blocked", data)
         }
     })
-
-
+    
     socket.on("typing", ({chatRoomId, senderId}: {chatRoomId: string, senderId: string}) => {
         socket.in(chatRoomId).emit("typing", ({ chatRoomId, senderId}))
         console.log('typing happened');
     })
+
+    // call related events starting
+
+    socket.on("call-user", (data) => {
+        console.log('call user in backend');
+        console.log(data);   
+        socket.to(data?.to).emit("calling-user", data)      
+    })
+
+    socket.on('call-accepted', (data) => {
+        console.log(`call accepted `);
+        console.log(data);        
+        socket.in(data.to).emit("receiver-accepted", data.signalData);
+    });
+    
+    socket.on("call-rejected", (data) => {
+        console.log("call rejected");
+        console.log(data);
+        socket.to(data?.to).emit("call-rejected", data)      
+    })
+
+    socket.on("call-cancelled", (data) => {
+        console.log('call rejected');
+        console.log(data);
+        socket.to(data?.to).emit("call-cancelled", data)      
+    })
+
+    // call related events ending
 }
